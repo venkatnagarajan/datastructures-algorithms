a6a520bf5d9f5c4d27326851e35d6eb05dc7c41f
venkatnagarajan <nvraman81@gmail.com>
2020-01-07 12:15:36 +0530

    add bubble sort file

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort{
+	public static void main(String args[]){
+		int[] inputArr = {20, 27, -22, 3, 52, 15, 12};
+		int temp;
+		
+		for (int j=inputArr.length-1;j>0;j--){
+			for (int i=0;i<j;i++){
+				if (inputArr[i] > inputArr[i+1]){
+					temp = inputArr[i+1];
+					inputArr[i+1] = inputArr[i];
+					inputArr[i] = temp;
+				}
+			}
+		}
+		for (int i : inputArr){
+			System.out.println(i);
+		}
+	}
+
+}
