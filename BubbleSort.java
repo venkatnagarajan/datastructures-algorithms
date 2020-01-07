24bd5aaf696df9bdc28ab17f8876d974db83f31a
venkatnagarajan <nvraman81@gmail.com>
2020-01-07 12:24:50 +0530

    add comments to BubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,7 +2,7 @@ public class BubbleSort{
 	public static void main(String args[]){
 		int[] inputArr = {20, 27, -22, 3, 52, 15, 12};
 		int temp;
-		
+		// iterate through the list n-1 times. During each iteration the largest elements bubbles up to the right end.
 		for (int j=inputArr.length-1;j>0;j--){
 			for (int i=0;i<j;i++){
 				if (inputArr[i] > inputArr[i+1]){
